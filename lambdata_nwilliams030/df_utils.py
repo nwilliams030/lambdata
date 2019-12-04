3239ca15881bbe491ea71d13c9578fca6b686dc3
nwilliams030 <nwilliams030@users.noreply.github.com>
2019-12-04 17:17:34 -0500

    final product

--- lambdata_nwilliams030/df_utils.py
+++ lambdata_nwilliams030/df_utils.py
@@ -2,3 +2,22 @@
 
 import pandas
 TEST_DF = pandas.DataFrame([1,2,3,4,5,6])
+
+#Function to split dates ("MM/DD/YYYY", etc.) into multiple columns
+def split_dates(df):
+    df = df.copy()
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day
+    return df
+
+def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(df.index)
+    m = len(df.index)
+    train_end = int(train_percent * m)
+    validate_end = int(validate_percent * m) + train_end
+    train = df.iloc[perm[:train_end]]
+    validate = df.iloc[perm[train_end:validate_end]]
+    test = df.iloc[perm[validate_end:]]
+    return train, validate, test
